917cbc3f229c56e76e255ebb486f4668223342e3
mohammadhossainjoy <joyh2755@gmail.com>
2025-08-23 01:57:31 +0600

    feat(db): add EF6, AppDbContext, and initial migration

---
+++ StudentTaskTracker/StudentTaskTracker/Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentTaskTracker.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(300)]
+        public string Notes { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
